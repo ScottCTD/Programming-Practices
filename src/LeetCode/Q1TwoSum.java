5a66ddaf2ddece13538d13078dd2eb09ea614a15
ScottCTD <ScottCTD@outlook.com>
2020-12-21 20:47:13 +0800

    Conclude Q1

--- src/LeetCode/Q1TwoSum.java
+++ src/LeetCode/Q1TwoSum.java
@@ -13,6 +13,11 @@ public class Q1TwoSum {
         System.out.println(Arrays.toString(twoSum02(new int[]{3, 2, 4}, 6)));
     }
 
+    /*
+    Original
+    Very Effective
+    My personal solutions in Java and C, using a very direct way of double for loops.
+     */
     public static int[] twoSum01(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -28,7 +33,12 @@ public class Q1TwoSum {
         return null;
     }
 
-    // Map
+    /*
+    Map
+    这个方法用一个Map来存储数组当中的key和value的，这里为了方便，把数组内的value设置为map的key，而index为value。
+    这个时候，如果这个map里面有target - nums[i]对应的value，就代表这俩map中的value是答案，然后每一次循环都把这个循环的东西放进去。时间复杂度为O(n)。
+    同时，这个方法也满足不重复用数的规则。
+     */
     private static int[] twoSum02(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
