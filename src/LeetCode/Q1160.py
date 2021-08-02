ae36ce8f0aabbb02bd820cd40483e4044aec08a9
ScottCTD <ScottCTD@outlook.com>
2021-08-03 00:28:47 +0800

    Add Q237 Q1160 Q1342

---
+++ src/LeetCode/Q1160.py
@@ -0,0 +1,35 @@
+# You are given an array of strings words and a string chars.
+# A string is good if it can be formed by characters from chars (each character can only be used once).
+# Return the sum of lengths of all good strings in words.
+
+class Solution:
+
+    # Original
+    # 96.99%
+    def countCharacters(self, words: list[str], chars: str) -> int:
+        result = 0
+        char_map = {}
+        for c in chars:
+            if c in char_map:
+                char_map[c] += 1
+            else:
+                char_map[c] = 1
+        for s in words:
+            good = True
+            temp = char_map.copy()
+            for c in s:
+                if c not in temp:
+                    good = False
+                    break
+                elif temp[c] == 1:
+                    del temp[c]
+                else:
+                    temp[c] -= 1
+            if good:
+                result += len(s)
+        return result
+
+
+if __name__ == "__main__":
+    print(Solution().countCharacters(["cat","bt","hat","tree"], "atach") == 6)
+    print(Solution().countCharacters(["hello","world","leetcode"], "welldonehoneyr") == 10)
---
+++ src/LeetCode/Q1342.py
@@ -0,0 +1,23 @@
+# Given an integer num, return the number of steps to reduce it to zero.
+# In one step, if the current number is even, you have to divide it by 2, otherwise, 
+# you have to subtract 1 from it.
+
+class Solution:
+
+    # 2021/08/03
+    # Original
+    # 99.4%
+    def numberOfSteps(self, num: int) -> int:
+        result = 0
+        while num > 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            result += 1
+        return result
+
+if __name__ == "__main__":
+    print(Solution().numberOfSteps(14) == 6)
+    print(Solution().numberOfSteps(8) == 4)
+    print(Solution().numberOfSteps(123) == 12)
