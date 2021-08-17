8786f5a68089ae4918573673ced176646ddf4df7
ScottCTD <ScottCTD@outlook.com>
2021-08-17 20:17:37 +0800

    Add Q344

---
+++ src/LeetCode/Q344.py
@@ -0,0 +1,26 @@
+# 344. Reverse String
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# Scott 2021/08/17
+
+from typing import List
+
+class Solution:
+
+    # Original
+    # 91.17%
+    # Time O(n)
+    # Space O(1)
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s) // 2):
+            c = s[i]
+            s[i] = s[-(i + 1)]
+            s[-(i + 1)] = c
+    
+
+if __name__ == '__main__':
+    s = ["h","e","l","l","o"]
+    Solution().reverseString(s)
+    print(s)
