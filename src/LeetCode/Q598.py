884e57ceb580b997b2cc5da5e8e08aab80f66a30
Scott <ScottCTD@outlook.com>
2022-01-15 20:40:49 -0500

    Add LeetCode Q598.

---
+++ src/LeetCode/Q598.py
@@ -0,0 +1,16 @@
+# 598. Range Addition II
+# You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+
+# Count and return the number of maximum integers in the matrix after performing all the operations.
+
+# Scott 2022/01/15
+
+from typing import List
+
+
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        for a, b in ops:
+            m = min(m, a)
+            n = min(n, b)
+        return m * n
