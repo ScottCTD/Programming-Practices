58324c4caa1845ad2efbedad56cde10c0e7dcfe8
Scott <ScottCTD@outlook.com>
2023-05-09 17:38:17 -0400

    [+] Added another method for Q54.

--- LeetCode/python/Q54.py
+++ LeetCode/python/Q54.py
@@ -1,5 +1,7 @@
 # Given an m x n matrix, return all elements of the matrix in spiral order.
 
+from typing import List
+
 class Solution:
 
     # Original
@@ -35,16 +37,60 @@ class Solution:
         return result
 
 
+class Solution2:
+
+    # Original
+    # 2023/05/09
+    # 20.88%
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        r = []
+        i, j = 0, 0
+        # offsets
+        offset = 0
+        d = [0, 1]
+        while len(r) != m * n:
+            r.append(matrix[i][j])
+            # collision: j == n - b and d == [0, 1]
+            #            i == m - a and d == [1, 0]
+            #            j == b and     d == [-1, 0]
+            #            i == a and     d == [0, -1]
+            if j == n - offset - 1 and d == [0, 1]:
+                pass
+            elif i == m - offset - 1 and d == [1, 0]:
+                pass
+            elif j == offset and d == [0, -1]:
+                offset += 1
+            elif i == offset and d == [-1, 0]:
+                pass
+            else:
+                i += d[0]
+                j += d[1]
+                continue
+            # change direction: [0, 1] -> [1, 0],
+            #                   [1, 0] -> [0, -1],
+            #                   [0, -1] -> [-1, 0],
+            #                   [-1, 0] -> [0, 1]
+            if d[0] == 0:
+                d[0], d[1] = d[1], d[0]
+            else:
+                d[0], d[1] = d[1], -d[0]
+            i += d[0]
+            j += d[1]
+        return r
+
+
 if __name__ == '__main__':
-    print(Solution().spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [
+    print(Solution2().spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [
           1, 2, 3, 6, 9, 8, 7, 4, 5])
-    print(Solution().spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]) == [
+    print(Solution2().spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]) == [
           1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7])
-    print(Solution().spiralOrder([[3], [2]]) == [3, 2])
-    print(Solution().spiralOrder([[1]]) == [1])
-    print(Solution().spiralOrder([[1], [2], [3]]) == [1, 2, 3])
-    print(Solution().spiralOrder([[1], [2], [3], [4]]) == [1, 2, 3, 4])
-    print(Solution().spiralOrder([[1], [2], [3], [4], [5]]) == [1, 2, 3, 4, 5])
-    print(Solution().spiralOrder([[]]) == [])
-    print(Solution().spiralOrder([[1, 2]]) == [1, 2])
-    print(Solution().spiralOrder([[1, 2], [3, 4]]) == [1, 2, 4, 3])
+    print(Solution2().spiralOrder([[3], [2]]) == [3, 2])
+    print(Solution2().spiralOrder([[1]]) == [1])
+    print(Solution2().spiralOrder([[1], [2], [3]]) == [1, 2, 3])
+    print(Solution2().spiralOrder([[1], [2], [3], [4]]) == [1, 2, 3, 4])
+    print(Solution2().spiralOrder([[1], [2], [3], [4], [5]]) == [1, 2, 3, 4, 5])
+    print(Solution2().spiralOrder([[]]) == [])
+    print(Solution2().spiralOrder([[1, 2]]) == [1, 2])
+    print(Solution2().spiralOrder([[1, 2], [3, 4]]) == [1, 2, 4, 3])
