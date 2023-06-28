0096fbf5c66f393087658090338d3c8caa50e6ff
Scott <ScottCTD@outlook.com>
2023-06-27 23:56:52 -0400

    [*] Updated LeetCode Q1514.

--- LeetCode/python/Q1514.py
+++ LeetCode/python/Q1514.py
@@ -1,4 +1,5 @@
 # 1514. Path with Maximum Probability
+# Medium
 # You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list
 # where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of
 # success of traversing that edge succProb[i].
