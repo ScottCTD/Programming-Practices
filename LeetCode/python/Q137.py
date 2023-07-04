d596b4a71fc693a946abf490a2648e9013cb6de2
Scott <ScottCTD@outlook.com>
2023-07-04 14:04:03 -0400

    [+] Updated LeetCode Q137.

--- LeetCode/python/Q137.py
+++ LeetCode/python/Q137.py
@@ -10,6 +10,21 @@ from typing import List
 # 2023-07-04 14:02:52
 # learned
 # very smart bit manipulation
+# count the # of ones in binary representation
+#
+# 3 times
+#
+# we need 2 bits 00, 01, 10, 11
+# we can select only 3 (00, 10, 01)
+#
+# use two ints to represent the count
+#
+# i1 and i2, for 01, i1 corresponds to 0, i2 corresponds to 1
+#
+# if i1 and input is different, then we want to and with ~i2 to get the answer
+#
+# i1 = (i1 ^ input) & ~i2
+# i2 = (i2 ^ input) & ~i1
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         i1, i2 = 0, 0
